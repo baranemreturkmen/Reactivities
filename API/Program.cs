630097229dd51f46deaa248e26d58ca70f710f2c
Baran Emre Turkmen <baranemre1997@gmail.com>
2024-05-12 15:14:08 +0300

    gitignore eklendi. DbContext yapısı eklendi.

--- API/Program.cs
+++ API/Program.cs
@@ -11,7 +11,7 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddDbContext<DataContext>(opt => {
     opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
-});
+}); 
 
 var app = builder.Build();
 
