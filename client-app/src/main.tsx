448f89f21c07173911fb442ceeb5091aa69b5f86
Baran Emre Turkmen <baranemre1997@gmail.com>
2024-08-09 18:43:20 +0300

    Router installed and Routes.tsx added. vite.svg deleted.

--- client-app/src/main.tsx
+++ client-app/src/main.tsx
@@ -4,11 +4,13 @@ import App from './app/layout/App.js'
 import 'semantic-ui-css/semantic.min.css'
 import './app/layout/styles.css'
 import { store, StoreContext } from './app/stores/store.js'
+import { RouterProvider } from 'react-router-dom'
+import { router } from './app/router/Routes.js'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <StoreContext.Provider value={store}>
-      <App />
+     <RouterProvider router={router}></RouterProvider>
       </StoreContext.Provider>
   </React.StrictMode>,
 )
