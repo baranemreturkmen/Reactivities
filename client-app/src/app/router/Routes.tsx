8aba565f40e57e60d0e03b3a8bdeccfad78542c4
Baran Emre Turkmen <baranemre1997@gmail.com>
2024-08-11 09:47:51 +0300

    routes and react router dom added.

--- client-app/src/app/router/Routes.tsx
+++ client-app/src/app/router/Routes.tsx
@@ -1,10 +1,18 @@
 import { RouteObject, createBrowserRouter } from "react-router-dom";
 import App from "../layout/App";
+import HomePage from "../../features/activities/home/HomePage";
+import ActivityDashboard from "../../features/activities/dashboard/ActivityDashboard";
+import ActivityForm from "../../features/activities/form/ActivityForm";
 
 export const routes: RouteObject[] = [
     {
         path: "/",
-        element: <App/>
+        element: <App/>,
+        children: [
+            {path: '', element: <HomePage></HomePage>},
+            {path: 'activities', element: <ActivityDashboard></ActivityDashboard>},
+            {path: 'createActivity', element: <ActivityForm></ActivityForm>},
+        ]
     }
 ]
 
