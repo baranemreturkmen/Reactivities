2682ccdeba14bb86e099d8ba6e914f4e5d9228c3
Baran Emre Turkmen <baranemre1997@gmail.com>
2024-08-20 11:39:21 +0300

    ActivityForm fix

--- client-app/src/features/activities/form/ActivityForm.tsx
+++ client-app/src/features/activities/form/ActivityForm.tsx
@@ -2,14 +2,16 @@ import { Button, Form, Segment } from "semantic-ui-react"
 import { ChangeEvent, useEffect, useState } from "react";
 import { useStore } from "../../../app/stores/store";
 import { observer } from "mobx-react-lite";
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 import LoadingComponent from "../../../app/layout/LoadingComponent";
+import { v4 as uuid } from 'uuid';
 
 const ActivityForm = () => {
 
   const { activityStore } = useStore();
   const { createActivity, updateActivity, loading, loadingInitial, loadActivity } = activityStore;
   const { id } = useParams();
+  const navigate = useNavigate();
 
   const [activity, setActivity] = useState({
     id: '',
@@ -22,11 +24,17 @@ const ActivityForm = () => {
   })
 
   useEffect(() => {
-    if (id) loadActivity(id).then(activity => setActivity(activity!))
-    })
+    if (id) loadActivity(id).then(activity => setActivity(activity!))//Activity will always filled, ! with guaranteed
+    },[id,loadActivity])
 
 function handleSubmit() {
-  activity.id ? updateActivity(activity) : createActivity(activity);
+  if(!activity.id){
+    activity.id = uuid();
+    createActivity(activity).then(() => navigate(`/activities/${activity.id}`))
+  }
+  else{
+    updateActivity(activity).then(() => navigate(`/activities/${activity.id}`))
+  }
 }
 
 function handleInputChange(event: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) {
